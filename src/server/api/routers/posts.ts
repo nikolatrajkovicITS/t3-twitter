29b1e4921f7ef73b6f959b6876042d23f6baa6fd
Nikola <nikolatrajkovic.contact@gmail.com>
2023-09-17 16:59:18 +0200

    feat: Fetch users and posts, display them on main page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,14 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
 import { z } from "zod";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+};
 
 /**
  * @createTRPCRouter used to create a router for a specific resource
@@ -7,7 +17,27 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log("usersusers", users);
+
+    return posts.map((post) => {
+      const user = users.find((user) => user.id === post.authorId);
+
+      return {
+        post,
+        author: user,
+      };
+    });
   }),
 });
